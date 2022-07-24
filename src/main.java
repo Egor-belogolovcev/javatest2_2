8c4227d07227603062e2d04803a17fe516807365
Егор Белоголовцев <egor_belogolovcev@mail.ru>
2022-07-24 22:47:52 +0300

    add gitignore

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args) {
+        int amountMoney = 1; //кол-во денег на счету
+        int elementaryScore = 100; //начальный счет
+        int amountPayment = 1100; //сумма пополнения
+        boolean bonus = amountPayment >= 1000;
+
+        int amountBonus; //кол-во бонусов
+        if (bonus) {
+            amountBonus = amountPayment / 100;
+        } else {
+            amountBonus = 0;
+        }
+        int finalScore = elementaryScore + amountPayment + amountBonus; //итоговый счет
+        System.out.println(finalScore);
+    }
+}
